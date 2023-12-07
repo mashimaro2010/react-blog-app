f1854046b491bd8f754c6d0a937706320a314fe2
mashimaro2010 <amnat.kaewtunkum@gmail.com>
2023-12-07 12:25:35 +0700

    เรียนรู้การใช้ Navigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Blogs from "./components/Blogs";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import NotFound from "./components/NotFound";
 function App() {
@@ -13,6 +13,8 @@ function App() {
           <Route path="/about" element={<About/>}></Route>
           <Route path="/blogs" element={<Blogs/>}></Route>
           <Route path="*" element={<NotFound/>}></Route>
+          <Route path="/home" element={<Navigate to="/"/>}></Route>
+          <Route path="/info" element={<Navigate to="/about"/>}></Route>
       </Routes>
     </BrowserRouter>
   );
